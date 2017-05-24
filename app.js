c4b2e9112ea5dbf992f92373e23f88a587e1d556
Gabriel Doty <gdoty@adconion.com>
2017-05-23 23:12:39 -0700

    Incremental

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // content of index.js
 const http = require('http');
 const path = require('path');
+const request = require('request');
+const async = require('async');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -21,6 +23,105 @@ app.get(
         response.render('reporting');
     }
 );
+
+app.get(
+    '/getUsers', (req, res) => {
+
+        var urls = [
+            'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984210&response-formatter=json',
+            'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984208&response-formatter=json',
+            'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984214&response-formatter=json',
+            'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984213&response-formatter=json',
+            'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984487&response-formatter=json',
+            'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984488&response-formatter=json',
+            'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984489&response-formatter=json',
+            'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984490&response-formatter=json'
+        ];
+
+        function requester(url, done) {
+            console.log('request: %s', url);
+            request(
+                url, function(err, r, body) {
+                    if (err) {
+                        return done(err);
+                    }
+                    done(null, JSON.parse(body));
+                }
+            )
+        }
+
+        async.map(
+            urls, requester, function(err, r) {
+                if (err) {
+                    console.log('ERROR: ', err);
+                    return;
+                }
+                res.send(
+                    {
+                        fashion0: r[0],
+                        fashion1: r[1],
+                        fashion2: r[2],
+                        fashion3: r[3],
+                        car0: r[4],
+                        car1: r[5],
+                        car2: r[6],
+                        car3: r[7]
+                    }
+                )
+            }
+        );
+    }
+);
+
+/**
+ app.get(
+ '/getUsers', (req, res) => {
+        async.map(
+            [
+                'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984210&response-formatter=json',
+                'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984208&response-formatter=json',
+                'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984214&response-formatter=json',
+                'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984213&response-formatter=json',
+                'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984487&response-formatter=json',
+                'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984488&response-formatter=json',
+                'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984489&response-formatter=json',
+                'http://prof084.sjc2.turn.com:4900/execute?command=command+streamer+&action=dump_users&turn_category_id=31984490&response-formatter=json'
+            ],
+            function(file, cb) {
+                request.get(
+                    file, function(err, response, body) {
+                        if (err) {
+                            cb(err);
+                        }
+                        else {
+                            cb(null, body); // First param indicates error, null=> no error
+                        }
+                    }
+                );
+            },
+            function(err, results) {
+                if (err) {
+                    res.send(err);
+                }
+                else {
+                    res.send(
+                        {
+                            fashion0: results[0],
+                            fashion1: results[1],
+                            fashion2: results[2],
+                            fashion3: results[3],
+                            auto1: results[4],
+                            auto2: results[5],
+                            auto3: results[6],
+                            auto4: results[7],
+                        }
+                    )
+                }
+            }
+        );
+    }
+ );
+ **/
 
 app.listen(
     port, (err) => {
--- public/js/home.controller.js
+++ public/js/home.controller.js
@@ -1,86 +1,165 @@
 'use strict';
 
-var width = 960,
-    height = 600,
-    padding = 6;
-
-var n = 100, // total number of nodes
-    m = 1; // number of distinct clusters
-
-var color = d3.scale.category10()
-    .domain(d3.range(m));
-
-var x = d3.scale.ordinal()
-    .domain(d3.range(m))
-    .rangePoints([0, width], 1);
-
-var nodes = d3.range(n).map(
+jQuery(
     function() {
-        var i = Math.floor(Math.random() * m),
-            v = (i + 1) / m * -Math.log(Math.random());
-        return {
-            radius: 5,
-            color: color(i),
-            cx: x(i),
-            cy: height / 2
-        };
+
     }
 );
+var width = 1170,
+    height = 1600,
+    padding = 12;
 
-var force = d3.layout.force()
-    .nodes(nodes)
-    .size([width, height])
-    .gravity(0)
-    .charge(0)
-    .on("tick", tick)
-    .start();
+var colors = [
+    '#999',
+    '#2ca02c'
+];
 
-var svg = d3.select("#chart").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var weightColors = [
+    '#4ca9b8',
+    '#4cb8b0',
+    '#4c67b8',
+    '#b388e0'
+];
 
-var circle = svg.selectAll("circle")
-    .data(nodes)
-    .enter().append("circle")
-    .attr(
-        "r", function(d) {
-            return d.radius;
-        }
-    )
-    .style(
-        "fill", function(d) {
-            return d.color;
-        }
-    )
-    .call(force.drag);
+var gravityPoints = [
+    {
+        id: 1,
+        y: 350,
+        x: 585
+    },
+    {
+        id: 2,
+        y: 380,
+        x: 220
+    },
+    {
+        id: 2,
+        y: 620,
+        x: 220
+    }
+];
 
-function tick(e) {
-    circle
-        .each(gravity(.1 * e.alpha))
-        .each(collide(.5))
-        .attr(
-            "cx", function(d) {
-                return d.x;
+var mainNodes = getNodes(1000, 160);
+var throttle = 0;
+var throttleMax = 5;
+var circles = runSimulation('#chart1', mainNodes);
+
+var activeNodes = [];
+var circleQueue = [];
+
+function activateCircle(node) {
+    var lowestScope = null;
+    var lowestItem = null;
+    var furthestScope;
+    var furthestItem;
+
+    circles.each(
+        function(d) {
+            if (d.state === 1) {
+                if (!lowestItem) {
+                    lowestScope = this;
+                    lowestItem = d;
+                }
+                else {
+                    if (d.py > lowestItem.py) {
+                        lowestScope = this;
+                        lowestItem = d;
+                    }
+                }
             }
-        )
-        .attr(
-            "cy", function(d) {
-                return d.y;
+            else if (d.state === 2) {
+                if (throttle > throttleMax) {
+                    throttle = 0;
+
+                    if (!furthestScope) {
+                        furthestScope = this;
+                        furthestItem = d;
+                    }
+                    else {
+                        if (d.px > furthestItem.px) {
+                            furthestScope = this;
+                            furthestItem = d;
+                        }
+                    }
+                }
             }
-        );
+        }
+    );
+
+    lowestItem.weight = node.weight;
+
+    if (!node.activated) {
+        switchCircleToPreBeacon(lowestScope, lowestItem);
+    }
+    else {
+        switchCircleToBeacon(lowestScope, lowestItem);
+    }
+}
+
+function switchCircleToPreBeacon(d, circle) {
+    circle.gravityPoint = gravityPoints[1];
+    circle.state = 2;
+    circle.radius = 10;
+
+    d3.select(d).style('fill', weightColors[circle.weight]);
+    d3.select(d).style('r', 10);
+
+    //mainNodes.push(getNode(0, 0, 0, 0));
+}
+
+function switchCircleToBeacon(d, circle) {
+    var color = d3.scale.category10().domain(d3.range(10));
+
+    circle.gravityPoint = gravityPoints[2];
+    circle.state = 3;
+
+    d3.select(d).style('fill', colors[2]);
+
+    //mainNodes.push(getNode(0, 0, 0, 0));
+}
+
+function getNodes(number, yGravity) {
+    var n = number, // total number of nodes
+        m = 1; // number of distinct clusters
+
+    return d3.range(n).map(
+        function() {
+            var i = Math.floor(Math.random() * m);
+
+            return getNode(i, m, yGravity);
+        }
+    );
+}
+
+function getNode(i, m, yGravity) {
+    var color = d3.scale.category10().domain(d3.range(m));
+
+    var x = d3.scale.ordinal()
+        .domain(d3.range(m))
+        .rangePoints([0, width], 1);
+
+    return {
+        radius: 6,
+        color: colors[0],
+        cx: x(i),
+        cy: yGravity,
+        gravityPoint: gravityPoints[0],
+        state: 1
+    };
 }
 
 // Move nodes toward cluster focus.
 function gravity(alpha) {
     return function(d) {
-        d.y += (d.cy - d.y) * alpha;
-        d.x += (d.cx - d.x) * alpha;
+        d.y += (d.gravityPoint.y - d.y) * alpha;
+        d.x += (d.gravityPoint.x - d.x) * alpha;
     };
 }
 
 // Resolve collisions between nodes.
-function collide(alpha) {
+function collide(alpha, nodes) {
     var quadtree = d3.geom.quadtree(nodes);
+
     return function(d) {
         var r = d.radius,
             nx1 = d.x - r,
@@ -95,8 +174,9 @@ function collide(alpha) {
                         y = d.y - quad.point.y,
                         l = Math.sqrt(x * x + y * y),
                         r = d.radius + quad.point.radius + (d.color !== quad.point.color) * padding;
+
                     if (l < r) {
-                        l = (l - r) / l * alpha;
+                        l = (l - r) / l * .1;
                         d.x -= x *= l;
                         d.y -= y *= l;
                         quad.point.x += x;
@@ -108,3 +188,203 @@ function collide(alpha) {
         );
     };
 }
+
+var force;
+
+function runSimulation(htmlId, nodes) {
+    var svg = d3.select(htmlId).append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    force = d3.layout.force()
+        .nodes(nodes)
+        .size([width, height])
+        .gravity(0)
+        .charge(0)
+        .on(
+            'tick', function(e) {
+                circles
+                    .each(gravity(.08 * e.alpha))
+                    .each(collide(.5, nodes))
+                    .attr(
+                        'cx', function(d) {
+                            return d.x;
+                        }
+                    )
+                    .attr(
+                        'cy', function(d) {
+                            return d.y;
+                        }
+                    );
+            }
+        )
+        .start();
+
+    setInterval(
+        function() {
+            force.alpha(0.1);
+        }, 250
+    );
+
+    return svg.selectAll('circle')
+        .data(nodes)
+        .enter().append('circle')
+        .attr(
+            'r', function(d) {
+                return d.radius;
+            }
+        )
+        .style(
+            'fill', function(d) {
+                return d.color;
+            }
+        )
+        .call(force.drag);
+}
+
+function processNewNodes(result) {
+    var nodesToAdd = [];
+
+    _.each(
+        result['fashion0'].rows, function(item) {
+            item.weight = 0;
+            nodesToAdd.push(item);
+        }
+    );
+    _.each(
+        result['fashion1'].rows, function(item) {
+            item.weight = 1;
+            nodesToAdd.push(item);
+        }
+    );
+    _.each(
+        result['fashion2'].rows, function(item) {
+            item.weight = 2;
+            nodesToAdd.push(item);
+        }
+    );
+    _.each(
+        result['fashion3'].rows, function(item) {
+            item.weight = 3;
+            nodesToAdd.push(item);
+        }
+    );
+
+    _.each(
+        nodesToAdd, function(node) {
+            var alreadyExists = _.find(
+                activeNodes, function(item) {
+                    return item[0] === node[0];
+                }
+            );
+
+            if (!alreadyExists) {
+                activeNodes.push(node);
+                circleQueue.push(node);
+            }
+        }
+    );
+
+    circleQueue = _.shuffle(circleQueue);
+}
+
+function getAllUsers() {
+    return {
+        then: function(callback) {
+            jQuery.get(
+                '/getUsers',
+                {},
+                function(data) {
+                    /**var data = {
+                fashion0: {
+                    "columns": ["user_id (for category_id=31984208)"],
+                    "warnings": [],
+                    "header": [],
+                    "rows": [
+                        ["3118213610035373959"],
+                        ["3737315022181373557"],
+                        ["2406378874252901713"],
+                        ["3190302140996869223"],
+                        ["3359252916128457505"],
+                        ["2843554010745620484"]
+                    ],
+                    "errors": [],
+                    "info": ["Number of users: 6"]
+                },
+                fashion1: {
+                    "columns": ["user_id (for category_id=31984208)"],
+                    "warnings": [],
+                    "header": [],
+                    "rows": [
+                        ["3118213g6100351373959"],
+                        ["37373150da22181373557"],
+                        ["240637fas8874252901713"],
+                        ["31903021409968asd69223"],
+                        ["3359252916128457505"],
+                        ["28435a540107456g20484"]
+                    ],
+                    "errors": [],
+                    "info": ["Number of users: 6"]
+                },
+                fashion2: {
+                    "columns": ["user_id (for category_id=31984208)"],
+                    "warnings": [],
+                    "header": [],
+                    "rows": [
+                        ["3118213a610035373959"],
+                        ["37373150221813173557"],
+                        ["2406378g874252901713"],
+                        ["319030214g0996869223"],
+                        ["33592529g161284a57505"],
+                        ["284355401g0745620484"]
+                    ],
+                    "errors": [],
+                    "info": ["Number of users: 6"]
+                },
+                fashion3: {
+                    "columns": ["user_id (for category_id=31984208)"],
+                    "warnings": [],
+                    "header": [],
+                    "rows": [
+                        ["3118213610g035373959"],
+                        ["3737315022181373557"],
+                        ["240637asd8874252901713"],
+                        ["3190302140996869223"],
+                        ["3359252916128457505"],
+                        ["2843554010asda745620484"]
+                    ],
+                    "errors": [],
+                    "info": ["Number of users: 6"]
+                }
+            };
+
+                     **/
+
+                    callback(data);
+                }
+            );
+        }
+    };
+}
+
+var pollInterval = setInterval(
+    function() {
+        getAllUsers().then(processNewNodes);
+    }, 5000
+);
+
+var queueInterval = setInterval(
+    function() {
+        if (circleQueue.length > 0) {
+            activateCircle(circleQueue[0]);
+
+            circleQueue.shift();
+        }
+    }, 500
+);
+
+setTimeout(
+    function() {
+        getAllUsers().then(processNewNodes);
+    }, 4000
+);
